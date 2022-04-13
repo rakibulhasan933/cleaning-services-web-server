fdf81f6fc5d57c67c02f1cc24fd514d3f84aaef8
rakibulhasan933 <rakibulmd933@gmail.com>
2022-04-13 21:45:29 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const dotenv = require('dotenv').config();
+
+
 const app = express()
-const port = 5000
+const port = `${process.env.PORT}`;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbxjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        // DATABASE COLLECTION PART
+        const database = client.db("cleaning");
+        const Services = database.collection("services");
+        console.log('Database connected');
+
+        // const result = await haiku.insertOne(doc);
+        // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Cleaning Services API Working!')
